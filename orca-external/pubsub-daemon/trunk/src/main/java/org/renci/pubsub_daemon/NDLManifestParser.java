0867faf400c56dd4f523b2d7dbe97c0ad83adc5a
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2013-08-30 17:59:02 +0000

    Interface changes in parser

--- orca-external/pubsub-daemon/trunk/src/main/java/org/renci/pubsub_daemon/NDLManifestParser.java
+++ orca-external/pubsub-daemon/trunk/src/main/java/org/renci/pubsub_daemon/NDLManifestParser.java
@@ -173,7 +173,7 @@ public class NDLManifestParser implements INdlManifestModelListener, INdlRequest
 	}
 
 	public void ndlNetworkConnectionPath(Resource c, OntModel m,
-			List<List<Resource>> path) {
+			List<List<Resource>> path, List<Resource> roots) {
 		
 	}
 	
