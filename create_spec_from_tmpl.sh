4b367b331665f3a1a9be91f553666fc3023b2869
Victor J. Orlikowski <vjo@duke.edu>
2015-05-20 17:37:49 -0400

    Fix up RPM building for GitHub

--- create_spec_from_tmpl.sh
+++ create_spec_from_tmpl.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-DATE=`date "+%Y%m%d"`
-GLOBALREV=`svnversion`
+DATE=`date "+%Y%m%d%H%M"`
+COMMIT=`git rev-parse HEAD`
+SHORTCOMMIT=`git rev-parse --short=8 HEAD`
 
 cp blowhole.spec.tmpl blowhole.spec
 
 sed -i -e "s;@@DATE@@;${DATE};" blowhole.spec
-sed -i -e "s;@@GLOBALREV@@;${GLOBALREV};" blowhole.spec
+sed -i -e "s;@@COMMIT@@;${COMMIT};" blowhole.spec
+sed -i -e "s;@@SHORTCOMMIT@@;${SHORTCOMMIT};" blowhole.spec
