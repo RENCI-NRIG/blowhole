65955250639c8dfb14a112c4691ab5930e2f17cb
Ilya Baldin <ibaldin@renci.org>
2016-02-01 13:57:04 -0500

    Latest

--- scripts/usage-reporting/environment.sh
+++ scripts/usage-reporting/environment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SMLIST="rci ufl uh tamu fiu ucd osf sl ciena wsu bbn psc nicta gwu umass wvn uaf"
-AMLIST="rci ufl uh tamu fiu ucd osf sl ciena wsu bbn psc nicta gwu umass wvn uaf"
+SMLIST="uvanl rci ufl uh tamu fiu ucd osf sl ciena wsu bbn psc nicta gwu umass wvn uaf"
+AMLIST="uvanl rci ufl uh tamu fiu ucd osf sl ciena wsu bbn psc nicta gwu umass wvn uaf"
+
+declare -A exceptions
+exceptions[uvanl]="uva-nl"
 
 PEQUOD=/home/ibaldin/bin/bin/pequod
 PEQUODCONFIG="/home/ibaldin/.pequod/properties"
@@ -11,6 +14,21 @@ function findUrl {
 	port=$2
 
 	RETURNURL=`grep $pattern $PEQUODCONFIG | grep $port | cut -f1 -d','`
+	if [ "$RETURNURL" = "" ]; then
+       		echo "Unable to find URL for $pattern:$port"
+        	exit 1
+	fi
+}
+
+function findUrlShort {
+	pattern=$1
+	port=$2
+
+	if [ "${exceptions[$pattern]}" != "" ]; then
+		pattern=${exceptions[$pattern]}
+	fi
+	hName=$pattern-hn.exogeni.net
+	RETURNURL=`grep $hName $PEQUODCONFIG | grep $port | cut -f1 -d','`
 	if [ "$RETURNURL" = "" ]; then
        		echo "Unable to find URL for $pattern:$port"
         	exit 1
